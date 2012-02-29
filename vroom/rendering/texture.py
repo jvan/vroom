e5d92674356e4a3d16b4a001a3451a8a0d2e063e
jvan <jvan@jvanlabs.net>
2012-02-29 15:58:47 -0800

    Added static methods to Texture class for initializing texture objects.

--- vroom/rendering/texture.py
+++ vroom/rendering/texture.py
@@ -7,7 +7,12 @@ from OpenGL.GL import *
 
 class Texture:
 
-   def __init__(self, filename):
+   def __init__(self):
+      self.texture = glGenTextures(1)
+   
+   @staticmethod
+   def from_file(filename):
+      print 'initializing texture from file'
       image = Image.open(filename)
 
       try:
@@ -15,10 +20,18 @@ class Texture:
       except SystemError:
          ix, iy, image = image.size[0], image.size[1], image.tostring("raw", "RGBX", 0, -1)
 
-      self.texture = glGenTextures(1) 
-      self.bind()
-      self.load(ix, iy, image)
-      self.unbind()
+      return Texture.from_data(ix, iy, image)
+
+   @staticmethod
+   def from_data(ix, iy, image):
+      print 'initializing texture from data'
+
+      texture = Texture()
+      texture.bind()
+      texture.load(ix, iy, image)
+      texture.unbind()
+      return texture
+
 
    def bind(self):
       glEnable(GL_TEXTURE_2D)
