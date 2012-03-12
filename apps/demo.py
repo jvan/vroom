d944718d847471384519380e3aaa39358676eaf7
jvan <jvan@jvanlabs.net>
2012-03-12 10:29:40 -0700

    Modified demo app to use Global.

--- apps/demo.py
+++ apps/demo.py
@@ -1,35 +1,38 @@
 #!/usr/bin/env vroom
+
 from vroom import * 
 
-modelAngles = [0.0, 0.0, 0.0]
-rotationalSpeeds = [9.0, -31.0, 19.0]
-animate = True
+# Functions
+
+def toggleRotate():
+   Global.animate = not Global.animate
+
+# vroom callbacks
 
 def init():
    setMainMenuTitle('Vroom Demo')
    addMainMenuItem('Reset ResetNavigation', centerDisplay)
    addMainMenuItem('Toggle Rotate', toggleRotate, type='toggle')
 
-def toggleRotate():
-   global animate
-   animate = not animate 
+   Global.animate = False
+   Global.modelAngles = [0.0, 0.0, 0.0]
 
 def draw():
    lighting(False);
    color(1.0)
 
    pushMatrix()
-   rotate(modelAngles)
-   cube(3.0)
+   rotate(Global.modelAngles)
+   cube(4.0)
    popMatrix()
 
 def frame():
-   if not animate:
+   if not Global.animate:
       return
 
    time = elapsedTime()
+   rotationalSpeeds = [9.0, -31.0, 19.0]
 
-   global modelAngles
-   modelAngles = [(angle+speed*time)%360.0 
-                     for (angle, speed) in zip(modelAngles, rotationalSpeeds)]
+   Global.modelAngles = [(angle+speed*time)%360.0 
+                     for (angle, speed) in zip(Global.modelAngles, rotationalSpeeds)]
 
