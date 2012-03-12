a95e2430adcdceaf06b1fa83daad611d2a9ac093
jvan <jvan@jvanlabs.net>
2012-03-12 10:30:38 -0700

    Modified points app to use Global.

--- apps/points.py
+++ apps/points.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env vroom
+
 from vroom import *
 
-NUM_POINTS = 10000
-
-vertices = list(random_vertex_generator(NUM_POINTS, -100.0, 100.0))
-colors = list(random_color_generator(NUM_POINTS))
-
-points = None
+# vroom callbacks
 
 def gl_init():
-   global points
-   points = PointCloud(vertices, colors)
-   points.sprite('share/particle.bmp')
+   NUM_POINTS = 10000
+   vertices = list(random_vertex_generator(NUM_POINTS, -100.0, 100.0))
+   colors = list(random_color_generator(NUM_POINTS))
+
+   Global.points = PointCloud(vertices, colors)
+
+   textureFile = get_resource('particle.bmp')
+   Global.points.sprite(textureFile)
 
 def draw():
    lighting(False)
-   points.draw()
+   Global.points.draw()
+
