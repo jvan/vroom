911a815a8fdbab21156837a3907671fda8110905
jvan <jvan@jvanlabs.net>
2012-03-12 10:28:16 -0700

    Modified button app to use point_list and Global.

--- apps/button.py
+++ apps/button.py
@@ -1,33 +1,23 @@
 #!/usr/bin/env vroom
+
 from vroom import *
 
-Lighting = True
+# Functions
 
-points = []
+def clearPoints():
+   Global.points = point_list()
+
+# vroom callbacks
 
 def init():
    addMainMenuItem('clear', clearPoints)
-
-def clearPoints():
-   global points
-   points = []
+   Global.points = point_list()
 
 def draw():
-   if not Lighting:
-      lighting(False)
-      color(0,1,0)
-   else:
-      material(0,1,0)
-
-   for p in points:
-      pushMatrix()
-      translate(p)
-      if Lighting:
-         sphere(1.0, style='solid')
-      else:
-         sphere(1.0)
-      popMatrix()
+   material(green)
+   Global.points.for_each(sphere, 1.0, style='solid')
 
 def button_press(pos):
    print ' -- button_press (pos={})'.format(pos)
-   points.append(pos)
+   Global.points.append(pos)
+
