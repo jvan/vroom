9283ed1fc7d78b1d719c77c64d652eb0bdbfc605
jvan <jvan@jvanlabs.net>
2012-03-12 10:32:41 -0700

    Modified randomwalk app to use Global.

--- apps/randomwalk.py
+++ apps/randomwalk.py
@@ -1,45 +1,39 @@
 #!/usr/bin/env vroom
+
 from vroom import *
 
-MAX_POINTS = 500
-
-points = [[0.0, 0.0, 0.0]]
-buffer = None
-
-colors = []
-color_map = ColorMap()
+# vroom callbacks
 
 def gl_init():
-   global points
-   global buffer
+   Global.MAX_POINTS = 500
+
+   Global.points = [[0.0, 0.0, 0.0] for i in range(Global.MAX_POINTS)] 
+
+   color_map = ColorMap()
+   Global.colors = [color_map(Global.MAX_POINTS-i, Global.MAX_POINTS) 
+         for i in range(Global.MAX_POINTS)]
    
-   buffer = GLArray(points)
-   buffer.renderMode(GL_LINE_STRIP)
+   Global.buffer = GLArray(Global.points)
+   Global.buffer.renderMode(GL_LINE_STRIP)
 
 def draw():
    lighting(False)
-   glEnable(GL_BLEND)
-   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
    lineWidth(3.0)
-   buffer.draw()
+   Global.buffer.draw()
    
 def frame():
-   global points
-   global buffer
-
-   current_pos = points[-1]
+   current_pos = Global.points[-1]
+   
    random_step = lambda: -1 if random() < 0.5 else 1
    steps = [random_step() for i in range(3)]
+
    next_pos = [pos+step for (pos, step) in zip(current_pos, steps)]
 
-   points.append(next_pos)
-   if len(points) > MAX_POINTS:
-      points.pop(0)
+   Global.points.append(next_pos)
 
-   global colors
-   count = len(points)
-   colors = [color_map(i, count) for i in range(count)]
-   
-   buffer = GLArray(points, colors)
-   buffer.renderMode(GL_LINE_STRIP)
+   if len(Global.points) > Global.MAX_POINTS:
+      Global.points.pop(0)
+
+   Global.buffer = GLArray(Global.points, Global.colors[:len(Global.points)])
+   Global.buffer.renderMode(GL_LINE_STRIP)
    
