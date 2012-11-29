ccb8d5ca7c52c70ffd022af0f49834d7103455d3
jvan <jvan@cse.ucdavis.edu>
2012-11-29 13:41:06 -0800

    Added documentation to code.

--- vroom/core/typography.py
+++ vroom/core/typography.py
@@ -1,20 +1,24 @@
-import FTGL
-from OpenGL.GL import *
+# System imports
 
+from OpenGL.GL import *
+import FTGL
+
+# Global font object
 _Font = None
 
-# processing syntax
-#
-# text(data, x, y)
-# text(data, x, y, z)
-# text(stringdata, x, y, width, height)
-# text(stringdata, x, y, width, height, z)
-
 def text(data, *args):
+   ''' Render text.
+
+   syntax:
+      text(data, x, y, z)
+      text(data [x, y, z])
+   '''
+
    if len(args) == 1:
       x, y, z = args[0]
    else:
       x, y, z = args
+
    _text_pixmap(data, x, y, z)
 
 def _text_pixmap(data, x, y, z):
@@ -25,7 +29,6 @@ def _text_pixmap(data, x, y, z):
    glPopMatrix()
 
 def _text_texture(data, x, y, z):
-   print ' rendering text={} at ({}, {}, {})'.format(data, x, y, z)
    glEnable(GL_TEXTURE_2D)
    glEnable(GL_BLEND)
    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
@@ -36,12 +39,13 @@ def _text_texture(data, x, y, z):
    glDisable(GL_TEXTURE_2D)
    glDisable(GL_BLEND)
 
-# processing syntax
-#
-# textFont(font)
-# textFont(font, size)
-
 def textFont(filename, size=10):
+   ''' Set the font type and size.'''
+
    global _Font
    _Font = FTGL.PixmapFont(filename)
+   textSize(size)
+
+def textSize(size):
+   global _Font
    _Font.FaceSize(size, 0)
