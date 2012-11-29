f9bb967d9dcec50b22105f017ff6076854a219d7
jvan <jvan@cse.ucdavis.edu>
2012-11-29 13:39:15 -0800

    Moved global variables and aliases to core/environment.py.

--- vroom/core/environment.py
+++ vroom/core/environment.py
@@ -1,4 +1,26 @@
-import pyvrui
+from OpenGL.GL import glPushMatrix, glPopMatrix
+from OpenGL.GL import glPointSize, glLineWidth
 
-elapsedTime = pyvrui.getCurrentFrameTime
+#import pyvrui
+from pyvrui import getCurrentFrameTime
+
+elapsedTime = getCurrentFrameTime
+
+pushMatrix = glPushMatrix
+popMatrix  = glPopMatrix
+pointSize  = glPointSize
+lineWidth  = glLineWidth
+
+# Global variables
+
+_App            = None  # the Application object
+_App_Path       = None  # path to the vroom application root directory
+_App_Fullpath   = None  # path to the vroom application 
+_Module_Name    = None  # name of the vroom module
+_Resource_Paths = None  # system resource directories
+
+_Listener       = None
+
+class _Global: pass
+Global = _Global()
 
