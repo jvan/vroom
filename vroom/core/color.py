228179474baa99ee3b5c71fc00ff38c8f9d60272
jvan <jvan@cse.ucdavis.edu>
2012-11-29 13:40:08 -0800

    Moved color definitions (red, green, blue, etc.) to core/color.py.

--- vroom/core/color.py
+++ vroom/core/color.py
@@ -3,6 +3,12 @@ from OpenGL.GL import glColor4fv
 import pyvrui
 from matplotlib import pylab
 
+red   = [1.0, 0.0, 0.0]
+green = [0.0, 1.0, 0.0]
+blue  = [0.0, 0.0, 1.0]
+white = [1.0, 1.0, 1.0]
+black = [0.0, 0.0, 0.0]
+
 class Color:
 
    def __init__(self, r=1.0, g=1.0, b=1.0, a=1.0):
